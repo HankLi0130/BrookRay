ec09899a516e25f0aa0031705c90551a535d11e4
Hank Li <HankLi0130@gmail.com>
2020-09-21 14:20:30 +0800

    Add Context extensions Add dimensions in dimens.xml Set min sdk to 24

--- core/src/main/java/tw/hankli/brookray/extension/ActivityX.kt
+++ core/src/main/java/tw/hankli/brookray/extension/ActivityX.kt
@@ -1,27 +1,25 @@
 package tw.hankli.brookray.extension
 
-import android.os.Build
+import android.app.Activity
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 /** Hide status bar */
-fun AppCompatActivity.hideStatusBar() =
+fun Activity.hideStatusBar() =
     window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
 /** Show status bar */
-fun AppCompatActivity.showStatusBar() =
+fun Activity.showStatusBar() =
     window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
 /** Show light status bar, only works equal or higher than SDK 23 (Android M) */
-fun AppCompatActivity.showLightStatusBar() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-    }
+fun Activity.showLightStatusBar() {
+    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 }
 
 /** Set status bar color */
-fun AppCompatActivity.setStatusBarColor(color: Int) {
+fun Activity.setStatusBarColor(color: Int) {
     window.statusBarColor = color
 }
 
---
+++ core/src/main/java/tw/hankli/brookray/extension/ContextX.kt
@@ -0,0 +1,31 @@
+package tw.hankli.brookray.extension
+
+import android.content.Context
+import android.content.DialogInterface
+import android.util.TypedValue
+import androidx.annotation.ArrayRes
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
+
+fun Context.getListDialog(
+    @StringRes titleId: Int,
+    @ArrayRes itemsId: Int,
+    listener: (DialogInterface, Int) -> Unit
+): AlertDialog {
+    return AlertDialog.Builder(this)
+        .setTitle(titleId)
+        .setItems(itemsId, listener)
+        .create()
+}
+
+// DP 轉 Pixel
+fun Context.dpToPx(dp: Float) =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
+
+// SP 轉 Pixel
+fun Context.spToPx(sp: Float) =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, resources.displayMetrics)
+
+// DP 轉 SP
+fun Context.dpToSp(dp: Float) =
+    (dpToPx(dp) / resources.displayMetrics.scaledDensity)
