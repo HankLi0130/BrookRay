713c1680bee0e4a4c1db3fca0dbfd76d362487d3
Hank Li <HankLi0130@gmail.com>
2021-05-28 14:05:14 +0800

    Update Log.kt

--- core/src/main/java/tw/hankli/brookray/core/log/Log.kt
+++ core/src/main/java/tw/hankli/brookray/core/log/Log.kt
@@ -6,4 +6,8 @@ const val TAG = "BrookRay"
 
 fun logInfo(message: String) = Log.i(TAG, message)
 
-fun logErr(message: String? = null, err: Throwable) = Log.e(TAG, message, err)
+fun logInfo(message: String?, e: Throwable?) = Log.i(TAG, message, e)
+
+fun logError(message: String) = Log.e(TAG, message)
+
+fun logError(message: String? = null, error: Throwable?) = Log.e(TAG, message, error)
