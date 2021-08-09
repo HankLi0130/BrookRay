96a76064ba9002b6282ba011aab017da7a178583
Hank Li <HankLi0130@gmail.com>
2021-08-09 20:16:00 +0800

    Add logWarn

--- core/src/main/java/tw/hankli/brookray/core/log/Log.kt
+++ core/src/main/java/tw/hankli/brookray/core/log/Log.kt
@@ -8,6 +8,8 @@ fun logInfo(message: String) = Log.i(TAG, message)
 
 fun logInfo(message: String?, e: Throwable?) = Log.i(TAG, message, e)
 
+fun logWarn(message: String) = Log.w(TAG, message)
+
 fun logError(message: String) = Log.e(TAG, message)
 
 fun logError(message: String? = null, error: Throwable?) = Log.e(TAG, message, error)
